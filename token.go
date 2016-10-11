4c21f05442fb977a38dcda19eb2a4d267f310d53
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-10-11 22:20:44 +0200

    simplified token

--- token.go
+++ token.go
@@ -19,23 +19,14 @@ type Token struct {
 	Signature []byte
 }
 
-// Note: The secret and to be generated token should both at least have a length
-// of 16 characters to be considered unguessable.
-func GenerateToken(secret []byte, length int) (*Token, error) {
-	// prepare key
-	key := make([]byte, length)
-
-	// read random bytes
-	_, err := io.ReadFull(randSource, key)
-	if err != nil {
-		return nil, err
-	}
-
+// Note: The secret and the token key should both at least have a
+// length of 16 characters to be considered unguessable.
+func TokenFromKey(secret []byte, key []byte) *Token {
 	// create hash
 	hash := hmac.New(sha256.New, secret)
 
 	// hash key - implementation does never return an error
-	_, err = hash.Write(key)
+	hash.Write(key)
 
 	// get signature
 	signature := hash.Sum(nil)
@@ -46,7 +37,22 @@ func GenerateToken(secret []byte, length int) (*Token, error) {
 		Signature: signature,
 	}
 
-	return token, nil
+	return token
+}
+
+// Note: The secret and the to be generated token key should both at least have a
+// length of 16 characters to be considered unguessable.
+func GenerateToken(secret []byte, length int) (*Token, error) {
+	// prepare key
+	key := make([]byte, length)
+
+	// read random bytes
+	_, err := io.ReadFull(randSource, key)
+	if err != nil {
+		return nil, err
+	}
+
+	return TokenFromKey(secret, key), nil
 }
 
 func ParseToken(secret []byte, str string) (*Token, error) {
@@ -59,36 +65,28 @@ func ParseToken(secret []byte, str string) (*Token, error) {
 	// decode key
 	key, err := b64.DecodeString(s[0])
 	if err != nil {
-		return nil, ErrorWithCode(InvalidRequest, "Key is not base64 encoded")
+		return nil, ErrorWithCode(InvalidRequest, "Token key is not base64 encoded")
 	}
 
 	// decode signature
 	signature, err := b64.DecodeString(s[1])
 	if err != nil {
-		return nil, ErrorWithCode(InvalidRequest, "Signature is not base64 encoded")
-	}
-
-	// create validation hash
-	validationHash := hmac.New(sha256.New, secret)
-
-	// hash key - implementation does never return an error
-	_, err = validationHash.Write(key)
-
-	// get signature
-	validationSignature := validationHash.Sum(nil)
-
-	// validate signatures
-	if !hmac.Equal(signature, validationSignature) {
-		return nil, ErrorWithCode(InvalidRequest, "Invalid token by not matching signature")
+		return nil, ErrorWithCode(InvalidRequest, "Token signature is not base64 encoded")
 	}
 
 	// construct token
-	token := &Token{
-		Key:       key,
-		Signature: signature,
+	token := TokenFromKey(secret, key)
+
+	// validate signatures
+	if !token.Equal(signature) {
+		return nil, ErrorWithCode(InvalidRequest, "Token key does not match signature")
 	}
 
 	return token, nil
+}
+
+func (t *Token) Equal(signature []byte) bool {
+	return hmac.Equal(t.Signature, signature)
 }
 
 func (t *Token) KeyString() string {
