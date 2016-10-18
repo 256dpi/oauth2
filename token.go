52eeb78c70634bea3b1ef48e1f18dc95e8d9a563
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-10-18 10:38:34 +0200

    more docs

--- token.go
+++ token.go
@@ -15,13 +15,18 @@ var randSource = rand.Reader
 
 var b64 = base64.RawURLEncoding
 
+// Token implements a simple abstraction around generating tokens using the
+// hmac-sha256 algorithm.
 type Token struct {
 	Key       []byte
 	Signature []byte
 }
 
-// Note: The secret and the token key should both at least have a
-// length of 16 characters to be considered unguessable.
+// TokenFromKey will return a new token that is constructed using the specified
+// secret and key.
+//
+// Note: The secret and the token key should both at least have a length of 16
+// characters to be considered unguessable.
 func TokenFromKey(secret []byte, key []byte) *Token {
 	// create hash
 	hash := hmac.New(sha256.New, secret)
@@ -41,8 +46,11 @@ func TokenFromKey(secret []byte, key []byte) *Token {
 	return token
 }
 
-// Note: The secret and the to be generated token key should both at least have a
-// length of 16 characters to be considered unguessable.
+// GenerateToken will return a new token that is constructed using the specified
+// secret and random key of the specified length.
+//
+// Note: The secret and the to be generated token key should both at least have
+// a length of 16 characters to be considered unguessable.
 func GenerateToken(secret []byte, length int) (*Token, error) {
 	// prepare key
 	key := make([]byte, length)
@@ -56,6 +64,7 @@ func GenerateToken(secret []byte, length int) (*Token, error) {
 	return TokenFromKey(secret, key), nil
 }
 
+// ParseToken will parse a token that is in its string representation.
 func ParseToken(secret []byte, str string) (*Token, error) {
 	// split dot separated key and signature
 	s := strings.Split(str, ".")
@@ -89,22 +98,31 @@ func ParseToken(secret []byte, str string) (*Token, error) {
 	return token, nil
 }
 
+// Valid returns true when the tokens key matches its signature.
 func (t *Token) Valid(secret []byte) bool {
 	return TokenFromKey(secret, t.Key).Equal(t.Signature)
 }
 
+// Equal returns true then the specified signature is the same as the tokens
+// signature.
+//
+// Note: This method should be used over just comparing the byte slices as it
+// computed in constant time and limits certain attacks.
 func (t *Token) Equal(signature []byte) bool {
 	return hmac.Equal(t.Signature, signature)
 }
 
+// KeyString returns a string (base64) representation of the key.
 func (t *Token) KeyString() string {
 	return b64.EncodeToString(t.Key)
 }
 
+// SignatureString returns a string (base64) representation of the signature.
 func (t *Token) SignatureString() string {
 	return b64.EncodeToString(t.Signature)
 }
 
+// String returns a string representation of the whole token.
 func (t *Token) String() string {
 	return fmt.Sprintf("%s.%s", t.KeyString(), t.SignatureString())
 }
