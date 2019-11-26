b37c69988b4556ffc7354c29762693e1eb8df5ce
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-11-26 10:42:09 +0100

    improved matrix tests

--- request_test.go
+++ request_test.go
@@ -55,27 +55,49 @@ func TestParseTokenRequestErrors(t *testing.T) {
 	r1, _ := http.NewRequest("GET", "", nil)
 	r2, _ := http.NewRequest("POST", "", nil)
 
-	matrix := []*http.Request{
-		r1,
-		r2,
-		newRequest(nil),
-		newRequest(map[string]string{
-			"grant_type": PasswordGrantType,
-		}),
-		newRequestWithAuth("foo", "bar", map[string]string{
-			"grant_type":   PasswordGrantType,
-			"redirect_uri": "blaa%blupp",
-		}),
-		newRequestWithAuth("foo", "bar", map[string]string{
-			"grant_type":   PasswordGrantType,
-			"redirect_uri": "foo",
-		}),
+	matrix := []struct {
+		r *http.Request
+		e string
+	}{
+		{
+			r: r1,
+			e: "invalid_request: invalid HTTP method",
+		},
+		{
+			r: r2,
+			e: "invalid_request: malformed query parameters or body form",
+		},
+		{
+			r: newRequest(nil),
+			e: "invalid_request: missing grant type",
+		},
+		{
+			r: newRequest(map[string]string{
+				"grant_type": PasswordGrantType,
+			}),
+			e: "invalid_request: missing or invalid HTTP authorization header",
+		},
+		{
+			r: newRequestWithAuth("foo", "bar", map[string]string{
+				"grant_type":   PasswordGrantType,
+				"redirect_uri": "blaa%blupp",
+			}),
+			e: "invalid_request: invalid redirect URI",
+		},
+		{
+			r: newRequestWithAuth("foo", "bar", map[string]string{
+				"grant_type":   PasswordGrantType,
+				"redirect_uri": "foo",
+			}),
+			e: "invalid_request: invalid redirect URI",
+		},
 	}
 
 	for _, i := range matrix {
-		req, err := ParseTokenRequest(i)
+		req, err := ParseTokenRequest(i.r)
 		assert.Nil(t, req)
 		assert.Error(t, err)
+		assert.Equal(t, i.e, err.Error())
 	}
 }
 
--- revocation/revocation_test.go
+++ revocation/revocation_test.go
@@ -55,19 +55,35 @@ func TestParseRequestErrors(t *testing.T) {
 	r1, _ := http.NewRequest("GET", "", nil)
 	r2, _ := http.NewRequest("POST", "", nil)
 
-	matrix := []*http.Request{
-		r1,
-		r2,
-		newRequest(nil),
-		newRequest(map[string]string{
-			"token": "foo",
-		}),
+	matrix := []struct {
+		r *http.Request
+		e string
+	}{
+		{
+			r: r1,
+			e: "invalid_request: invalid HTTP method",
+		},
+		{
+			r: r2,
+			e: "invalid_request: malformed query parameters or body form",
+		},
+		{
+			r: newRequest(nil),
+			e: "invalid_request: missing token",
+		},
+		{
+			r: newRequest(map[string]string{
+				"token": "foo",
+			}),
+			e: "invalid_request: missing or invalid HTTP authorization header",
+		},
 	}
 
 	for _, i := range matrix {
-		req, err := ParseRequest(i)
+		req, err := ParseRequest(i.r)
 		assert.Nil(t, req)
 		assert.Error(t, err)
+		assert.Equal(t, i.e, err.Error())
 	}
 }
 
